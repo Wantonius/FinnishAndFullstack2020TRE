99e02a627fe614a81476b2a9e6d223300264bb16
Wantonius <hentonen@gmail.com>
2020-05-28 10:05:03 +0300

    first hooks

---
+++ react/hooks/effect-hook/src/EffectHook.js
@@ -0,0 +1,36 @@
+import React,{useState,useEffect} from 'react';
+
+const EffectHook = () => {
+	const [isOn,setIsOn] = useState(false);
+	const [timer,setTimer] = useState(0)
+
+	useEffect(
+		() => {
+			let interval;
+			if(isOn) {
+				interval = setInterval(() => setTimer(timer => timer + +1),1000)
+			}
+			return () => clearInterval(interval);
+		},
+	[isOn])
+	
+	const reset = () => {
+		setIsOn(false);
+		setTimer(0);
+	}
+	
+	let button = <button onClick={() => setIsOn(true)}>Start</button>
+	if(isOn) {
+		button = <button onClick={() => setIsOn(false)}>Stop</button>
+	}
+	
+	return(
+		<div>
+			<p>Timer:{timer}</p>
+				{button}
+			<button disabled={0===true} onClick={reset}>Reset</button>
+		</div>
+	)
+}
+
+export default EffectHook;
